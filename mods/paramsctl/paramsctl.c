b7ed2ae8e26f5d3e46a1bd9457dfc5451ed03c3e
lidq <lidq@localhost.localdomain>
2017-07-21 15:11:28 +0800

    .

--- mods/paramsctl/paramsctl.c
+++ mods/paramsctl/paramsctl.c
@@ -102,13 +102,13 @@ void params_from_cache()
 void params_reset()
 {
 	// XY轴欧拉角PID参数
-	p->kp = 23.0;
+	p->kp = 32.0;
 	p->ki = 1.2;
-	p->kd = 60.0;
+	p->kd = 53.0;
 	// XY轴欧拉角PID参数
-	p->v_kp = 9.0;
+	p->v_kp = 8.0;
 	p->v_ki = 0.0;
-	p->v_kd = 13.0;
+	p->v_kd = 15.0;
 	//摇控器3通道起始值
 	p->ctl_fb_zero = 1500;
 	p->ctl_lr_zero = 1500;
