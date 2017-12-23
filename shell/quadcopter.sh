5997d01b922387256b2440141e156dbf412396dc
lidq <ldq0183@woozoom.net>
2017-12-23 13:49:23 +0800

    将shell中启动后的进程调整到实时最高的实时优先级

--- shell/quadcopter.sh
+++ shell/quadcopter.sh
@@ -16,7 +16,8 @@ case $1 in
 	start)
 		
 		release/bin/quadcopter --fly > /dev/null &
-		ps -ef | grep quadcopter | grep -v grep | awk '{print $2}' | xargs renice -19 > /dev/null &
+		ps -ef | grep quadcopter | grep -v grep | awk '{print $2}' | xargs chrt -p 99 > /dev/null &
+		#ps -ef | grep quadcopter | grep -v grep | awk '{print $2}' | xargs renice -19 > /dev/null &
 		#raspivid -o $(date +"%Y-%m-%d_%H-%M-%S").h264 -t 1200000 -w 1280 -h 1024 &
 		;;
 
@@ -27,7 +28,8 @@ case $1 in
 	restart)
 		ps -ef | grep "release/bin/quadcopter --fly" | grep -v grep | awk '{print $2}' | xargs kill -9
 		release/bin/quadcopter --fly > /dev/null &
-		ps -ef | grep quadcopter | grep -v grep | awk '{print $2}' | xargs renice -19 > /dev/null &
+		ps -ef | grep quadcopter | grep -v grep | awk '{print $2}' | xargs chrt -p 99 > /dev/null &
+		#ps -ef | grep quadcopter | grep -v grep | awk '{print $2}' | xargs renice -19 > /dev/null &
 		#raspivid -o $(date +"%Y-%m-%d_%H-%M-%S").h264 -t 1200000 -w 1280 -h 1024 &
 		;;
 
