7495d50122505ae5c530d6b712e692673e46ad49
lidq <ldq0183@woozoom.net>
2018-01-22 17:25:37 +0800

    加入记录日志缓冲区

--- mods/logger/logger.c
+++ mods/logger/logger.c
@@ -13,12 +13,18 @@ s8 quad_home[MAX_PATH_NAME];
 
 pthread_t pthddr;
 s32 s = 0;
+s32 s1 = 0;
 s32 r = 0;
 pthread_t pthd;
 s_engine* e = NULL;
 s_params* p = NULL;
 FILE* fp = NULL;
-s8* buff = NULL;
+s_buff_rw brw;
+s8 logging = 0;
+u64 logtime = 0;
+struct timeval timer;
+sem_t sem_write_buff;
+sem_t sem_write_file;
 
 s32 __init(s_engine* engine, s_params* params)
 {
@@ -27,30 +33,34 @@ s32 __init(s_engine* engine, s_params* params)
 
 	r = 1;
 	s = 1;
+	s1 = 1;
+
+	sem_init(&sem_write_buff, 0, 1);
+	sem_init(&sem_write_file, 0, 0);
 
 	//读取环境变量，安装位置
 	config_env(quad_home, "QUAD_HOME");
 
-	time_t now;
-	struct tm *timenow;
-	time(&now);
-	timenow = localtime(&now);
-	s8 logname[MAX_PATH_NAME];
-	snprintf(logname, MAX_PATH_NAME, "%s/log/%04d-%02d-%02d_%02d-%02d-%02d.csv", quad_home, timenow->tm_year + 1900, timenow->tm_mon + 1, timenow->tm_mday, timenow->tm_hour, timenow->tm_min, timenow->tm_sec);
-	buff = malloc(BUFF_SIZE);
-	if (buff == NULL)
+	brw.pos_w = 0;
+	brw.pos_r = 0;
+	brw.rw_size = 0;
+	brw.buff_size = BUFF_SIZE;
+	brw.buff = malloc(BUFF_SIZE);
+	brw.data = malloc(WRITE_SIZE);
+
+	if (brw.buff == NULL)
 	{
-		printf("[ ERROR ] Malloc memory.\n");
+		printf("[ ERROR ] Logger malloc memory error.\n");
 		return -1;
 	}
-	fp = fopen(logname, "w+");
-	if (fp == NULL)
+	if (brw.data == NULL)
 	{
-		printf("[ ERROR ] Open log error.\n");
+		printf("[ ERROR ] Logger malloc memory error.\n");
 		return -1;
 	}
 
 	pthread_create(&pthddr, (const pthread_attr_t*) NULL, (void* (*)(void*)) &logger_run, NULL);
+	pthread_create(&pthddr, (const pthread_attr_t*) NULL, (void* (*)(void*)) &logger_write, NULL);
 
 	printf("[ OK ] Logger Init.\n");
 
@@ -59,36 +69,241 @@ s32 __init(s_engine* engine, s_params* params)
 
 s32 __destory(s_engine* e, s_params* p)
 {
-	if (fp != NULL)
-	{
-		fclose(fp);
-	}
-	if (buff != NULL)
-	{
-		free(buff);
-	}
 	r = 0;
 	return 0;
 }
 
 s32 __status()
 {
-	return s;
+	return s + s1;
+}
+
+void logger_start()
+{
+	time_t now;
+	struct tm *timenow;
+	time(&now);
+	timenow = localtime(&now);
+	s8 logname[MAX_PATH_NAME];
+	snprintf(logname, MAX_PATH_NAME, "%s/log/%04d-%02d-%02d_%02d-%02d-%02d.csv", quad_home, timenow->tm_year + 1900, timenow->tm_mon + 1, timenow->tm_mday, timenow->tm_hour, timenow->tm_min, timenow->tm_sec);
+	fp = fopen(logname, "w+");
+	if (fp == NULL)
+	{
+		printf("[ ERROR ] Open log error.\n");
+		return;
+	}
+	gettimeofday(&timer, NULL);
+	logtime = timer.tv_sec * 1000000 + timer.tv_usec;
+
+	s32 count = snprintf(brw.data, WRITE_SIZE, "START_TIME");
+	fwrite(brw.data, sizeof(u8), count, fp);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",E_LOCK");
+	fwrite(brw.data, sizeof(u8), count, fp);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",E_TX,E_TY,E_TZ");
+	fwrite(brw.data, sizeof(u8), count, fp);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",E_X,E_Y,E_Z");
+	fwrite(brw.data, sizeof(u8), count, fp);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",E_CTLMX,E_CTLMY");
+	fwrite(brw.data, sizeof(u8), count, fp);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",E_TGX,E_TGY,E_TGZ");
+	fwrite(brw.data, sizeof(u8), count, fp);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",E_GX,E_GY,E_GZ");
+	fwrite(brw.data, sizeof(u8), count, fp);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",E_AX,E_AY,E_AZ");
+	fwrite(brw.data, sizeof(u8), count, fp);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",E_V");
+	fwrite(brw.data, sizeof(u8), count, fp);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",E_XV_DEVI,E_YV_DEVI,E_ZV_DEVI");
+	fwrite(brw.data, sizeof(u8), count, fp);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",E_CTL_FB,E->CTL_LR,E->CTL_PW,E->CTL_MD,E->CTL_UD,E->CTL_DI");
+	fwrite(brw.data, sizeof(u8), count, fp);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",E_LOCK_STATUS,E_MODE");
+	fwrite(brw.data, sizeof(u8), count, fp);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",P_KP,P_KI,P_KD");
+	fwrite(brw.data, sizeof(u8), count, fp);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",P_V_KP,P_V_KI,P_V_KD");
+	fwrite(brw.data, sizeof(u8), count, fp);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",P_VZ_KP,P_VZ_KI,P_VZ_KD");
+	fwrite(brw.data, sizeof(u8), count, fp);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",P_CTL_FB_ZERO,P_CTL_LR_ZERO,P_CTL_PW_ZERO,P_CTL_MD_ZERO,P_CTL_UD_ZERO,P_CTL_DI_ZERO");
+	fwrite(brw.data, sizeof(u8), count, fp);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",P_CTL_TYPE,P_CTL_DISPLAY");
+	fwrite(brw.data, sizeof(u8), count, fp);
+
+	count = snprintf(brw.data, WRITE_SIZE, "\n");
+	fwrite(brw.data, sizeof(u8), count, fp);
+}
+
+void logger_stop()
+{
+	if (fp != NULL)
+	{
+		fclose(fp);
+	}
+	if (brw.buff != NULL)
+	{
+		free(brw.buff);
+	}
+	if (brw.data != NULL)
+	{
+		free(brw.data);
+	}
+}
+
+void logger_write_data(int count)
+{
+	for (s32 i = 0; i < count; i++)
+	{
+		brw.buff[brw.pos_w++] = brw.data[i];
+		brw.rw_size++;
+		if (brw.pos_w >= brw.buff_size)
+		{
+			brw.pos_w = 0;
+		}
+	}
+}
+
+void logger_logging()
+{
+	sem_wait(&sem_write_buff);
+	gettimeofday(&timer, NULL);
+	u64 t = timer.tv_sec * 1000000 + timer.tv_usec;
+
+	s32 count = snprintf(brw.data, WRITE_SIZE, "%u", t - logtime);
+	logger_write_data(count);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",%d", e->lock);
+	logger_write_data(count);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",%f,%f,%f", e->tx, e->ty, e->tz);
+	logger_write_data(count);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",%f,%f,%f", e->x, e->y, e->z);
+	logger_write_data(count);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",%f,%f", e->ctlmx, e->ctlmy);
+	logger_write_data(count);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",%f,%f,%f", e->tgx, e->tgy, e->tgz);
+	logger_write_data(count);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",%f,%f,%f", e->gx, e->gy, e->gz);
+	logger_write_data(count);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",%f,%f,%f", e->ax, e->ay, e->az);
+	logger_write_data(count);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",%f", e->v);
+	logger_write_data(count);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",%f,%f,%f", e->xv_devi, e->yv_devi, e->zv_devi);
+	logger_write_data(count);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",%u,%u,%u,%u,%u,%u", e->ctl_fb, e->ctl_lr, e->ctl_pw, e->ctl_md, e->ctl_ud, e->ctl_di);
+	logger_write_data(count);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",%u,%u", e->lock_status, e->mode);
+	logger_write_data(count);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",%f,%f,%f", p->kp, p->ki, p->kd);
+	logger_write_data(count);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",%f,%f,%f", p->v_kp, p->v_ki, p->v_kd);
+	logger_write_data(count);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",%f,%f,%f", p->vz_kp, p->vz_ki, p->vz_kd);
+	logger_write_data(count);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",%u,%u,%u,%u,%u,%u", p->ctl_fb_zero, p->ctl_lr_zero, p->ctl_pw_zero, p->ctl_md_zero, p->ctl_ud_zero, p->ctl_di_zero);
+	logger_write_data(count);
+
+	count = snprintf(brw.data, WRITE_SIZE, ",%u,%u", p->ctl_type, p->ctl_display);
+	logger_write_data(count);
+
+	count = snprintf(brw.data, WRITE_SIZE, "\n");
+	logger_write_data(count);
+
+	if (brw.rw_size >= WRITE_SIZE)
+	{
+		sem_post(&sem_write_file);
+	}
+	sem_post(&sem_write_buff);
+}
+
+void logger_writting()
+{
+	sem_wait(&sem_write_file);
+	sem_wait(&sem_write_buff);
+	s32 pos_w = brw.pos_w;
+	s32 pos_r = brw.pos_r;
+	s32 rw_size = brw.rw_size;
+	brw.pos_r = brw.pos_w;
+	brw.rw_size = 0;
+	sem_post(&sem_write_buff);
+
+	if (pos_r < pos_w)
+	{
+		fwrite(brw.buff + pos_r, sizeof(u8), pos_w - pos_r, fp);
+	}
+	else if (pos_r > pos_w)
+	{
+		fwrite(brw.buff + pos_r, sizeof(u8), brw.buff_size - pos_r, fp);
+		fwrite(brw.buff, sizeof(u8), pos_w, fp);
+	}
 }
 
 void logger_run()
 {
 	while (r)
 	{
+		//start logger
+		if (!e->lock && !logging)
+		{
+			logger_start();
+			logging = 1;
+			printf("[ OK ] Logger started.\n");
+		}
+		//stop logger
+		if (e->lock && logging)
+		{
+			logger_stop();
+			logging = 0;
+			printf("[ OK ] Logger stoped.\n");
+		}
+		//logging
 		if (!e->lock)
 		{
-			s32 count = snprintf(buff, BUFF_SIZE, "%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n", e->v, e->ax, e->ay, e->az, e->gx + e->dgx, e->gy + e->dgy, e->gz + e->dgz, e->x + e->dx, e->y + e->dy, e->z + e->dz);
-			fwrite(buff, sizeof(s8), count, fp);
+			logger_logging();
 		}
 		usleep(ENG_TIMER * 1000);
 	}
 
 	s = 0;
+}
+
+void logger_write()
+{
+	while (r)
+	{
+		logger_writting();
+	}
+	s1 = 0;
 }
 
 int config_env(char* value, char *name)
--- mods/logger/logger.h
+++ mods/logger/logger.h
@@ -11,15 +11,38 @@
 
 #include <typedef.h>
 
-#define BUFF_SIZE	(2*0x400)
+#define BUFF_SIZE			(8*1024)
+#define WRITE_SIZE		(BUFF_SIZE/4)
 
-s32 __init(s_engine* e, s_params* p);
+typedef struct s_buff_rw
+{
+	u8 *buff;
+	u8 *data;
+	s32 pos_w;
+	s32 pos_r;
+	s32 rw_size;
+	s32 buff_size;
+} s_buff_rw;
+
+s32 __init(s_engine* engine, s_params* params);
 
 s32 __destory(s_engine* e, s_params* p);
 
 s32 __status();
 
+void logger_start();
+
+void logger_stop();
+
+void logger_write_data(int count);
+
+void logger_logging();
+
+void logger_writting();
+
 void logger_run();
+
+void logger_write();
 
 int config_env(char* value, char *name);
 
